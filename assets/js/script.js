3c04e6c5409dd32e5ecbee198167e1dda9ccca3b
JonBay <134355923+JonBay@users.noreply.github.com>
2023-10-18 08:16:31 -0600

    End quiz function added. Score,time, initials are stored.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,8 @@ var startButton = document.querySelector(".start-button");
 var timerCount;
 var timer;
 var timerElement = document.querySelector(".timer-left");
-var selectedOption; 
-
-
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-
-
+var selectedOption;
+var numberCorrect = 0;
 
 // startQuiz is called when the start button is clicked
 function startQuiz() {
@@ -17,8 +12,6 @@ function startQuiz() {
   firstQuestion();
 }
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-// I borrowed this function from the mini project we did and tweaked it to work.
 function startTimer() {
   // Sets timer
   timer = setInterval(function () {
@@ -26,19 +19,11 @@ function startTimer() {
     timerElement.textContent = timerCount;
     console.log(timerElement);
 
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
     // Tests if time has run out
     if (timerCount === 0) {
       // Clears interval
       clearInterval(timer);
-      loseGame();
+      outOfTime();
     }
   }, 1000);
 }
@@ -50,7 +35,6 @@ function firstQuestion() {
   showQuestion(1); // Show the first question
 }
 
-
 function showQuestion(questionNumber) {
   // Hide all questions
   for (let i = 1; i <= 4; i++) {
@@ -59,7 +43,8 @@ function showQuestion(questionNumber) {
 
   // Show the specified question
   document.getElementById(`question${questionNumber}`).style.display = "block";
-  document.getElementById(`question${questionNumber}`).style.textAlign = "center";
+  document.getElementById(`question${questionNumber}`).style.textAlign =
+    "center";
 
   // Clear any previous correct/incorrect messages
   document.getElementById("correct").style.display = "none";
@@ -68,9 +53,10 @@ function showQuestion(questionNumber) {
   attachEventListeners(questionNumber);
 }
 
-
 function attachEventListeners(questionNumber) {
-  document.getElementById(`question${questionNumber}`).querySelectorAll("button")
+  document
+    .getElementById(`question${questionNumber}`)
+    .querySelectorAll("button")
     .forEach(function (button) {
       button.addEventListener("click", function () {
         const isCorrect = button.getAttribute("data-correct") === "true";
@@ -80,79 +66,133 @@ function attachEventListeners(questionNumber) {
             document.getElementById("correct").style.display = "block";
             document.getElementById("correct").style.textAlign = "center";
             setTimeout(function () {
-              questionNumber ++; 
+              questionNumber++;
+              numberCorrect++;
               showQuestion(questionNumber);
-            }, 1000);  
+            }, 1000);
           } else {
             document.getElementById("incorrect").style.display = "block";
             document.getElementById("incorrect").style.textAlign = "center";
             timerCount -= 10; // Deduct 10 seconds for incorrect answer
             timerElement.textContent = timerCount; // Update timer display
             setTimeout(function () {
-              questionNumber ++; 
+              questionNumber++;
               showQuestion(questionNumber);
-            }, 1000); 
+            }, 1000);
           }
         } else if (questionNumber === 2) {
           if (isCorrect) {
             document.getElementById("correct").style.display = "block";
             document.getElementById("correct").style.textAlign = "center";
             setTimeout(function () {
-              questionNumber ++; 
+              questionNumber++;
+              numberCorrect++;
               showQuestion(questionNumber);
-            }, 1000); 
+            }, 1000);
           } else {
             document.getElementById("incorrect").style.display = "block";
             document.getElementById("incorrect").style.textAlign = "center";
             timerCount -= 10; // Deduct 10 seconds for incorrect answer
             timerElement.textContent = timerCount; // Update timer display
             setTimeout(function () {
-              questionNumber ++; 
+              questionNumber++;
               showQuestion(questionNumber);
-            }, 1000); 
+            }, 1000);
           }
         } else if (questionNumber === 3) {
           if (isCorrect) {
             document.getElementById("correct").style.display = "block";
             document.getElementById("correct").style.textAlign = "center";
             setTimeout(function () {
-              questionNumber ++; 
+              questionNumber++;
+              numberCorrect++;
               showQuestion(questionNumber);
-            }, 1000); 
+            }, 1000);
           } else {
             document.getElementById("incorrect").style.display = "block";
             document.getElementById("incorrect").style.textAlign = "center";
             timerCount -= 10; // Deduct 10 seconds for incorrect answer
             timerElement.textContent = timerCount; // Update timer display
             setTimeout(function () {
-              questionNumber ++; 
+              questionNumber++;
               showQuestion(questionNumber);
-            }, 1000); 
+            }, 1000);
           }
         } else if (questionNumber === 4) {
           if (isCorrect) {
             document.getElementById("correct").style.display = "block";
             document.getElementById("correct").style.textAlign = "center";
             setTimeout(function () {
-              questionNumber ++; 
-              showQuestion(questionNumber);
-            }, 1000); 
+              questionNumber++;
+              numberCorrect++;
+              endQuiz ();
+            }, 1000);
           } else {
             document.getElementById("incorrect").style.display = "block";
             document.getElementById("incorrect").style.textAlign = "center";
             timerCount -= 10; // Deduct 10 seconds for incorrect answer
             timerElement.textContent = timerCount; // Update timer display
             setTimeout(function () {
-              questionNumber ++; 
-              showQuestion(questionNumber);  // instead of showquestion here need function to end game
-            }, 1000); 
+              questionNumber++;
+              endQuiz ();
+            }, 1000);
           }
         }
       });
     });
 }
 
+function endQuiz() {
+  document.getElementById(`question4`).style.display = "none";
+  document.getElementById("correct").style.display = "none";
+  document.getElementById("incorrect").style.display = "none";
+  document.getElementById("quiz-over").style.display = "block";
+  document.getElementById("quiz-over").style.textAlign = "center";
+  document.getElementById("number-correct").style.display = "block";
+  document.getElementById("number-correct").style.textAlign = "center";
+  document.getElementById("number-correct").textContent = `You got ${numberCorrect} correct! `;
+  clearInterval(timer);
+  const timeTaken = 60 - timerCount; // Calculate time taken
+  document.getElementById("store-score").style.display = "block";
+  document.getElementById("store-score").style.textAlign = "center";
+  document.getElementById("initials").style.display = "block";
+  document.getElementById("initials").style.margin = "auto";
+  document.getElementById("submit").style.display = "block";
+  document.getElementById("submit").style.margin = "auto";
+  const userInitials = document.getElementById("initials").value;
+  storeUserData(timeTaken, numberCorrect, userInitials);
+}
+
+function storeUserData(timeTaken, numberCorrect, userInitials) {
+  const userData = {
+    time: timeTaken,
+    correctAnswers: numberCorrect,
+    initials: userInitials
+  };
+
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  highScores.push(userData);
+
+  // Sort the high scores
+  highScores.sort((a, b) => a.time - b.time);  //had internet help for this sort
+
+  // Store the high scores back to localStorage
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+
+// Make an outOfTime(); function.
+
 startButton.addEventListener("click", startQuiz);
 
 
 
+// to do's 
+// out of time function
+// event listener for submit button
+// New page to store high scores
+// retrieve high scores from local storage and display them 
+//link from first page to second page and back again 
+//readme
+//clean up code
+//submit
